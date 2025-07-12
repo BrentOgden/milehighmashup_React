1df039519c9ff951b9f79c1f6c20feced4e22489
Brent Ogden <ogden87@gmail.com>
2025-07-11 21:06:40 -0600

    Update Facts.jsx

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -26,7 +26,7 @@ export default function Facts() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 5000,
         pauseOnHover: true,
         arrows: false,
         adaptiveHeight: true,
